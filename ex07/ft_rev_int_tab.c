8526ed3a8178063dd60b8d33ce0e8dff8cbd9441
marciochamma <82783953+marciochamma@users.noreply.github.com>
2024-03-10 23:41:18 -0300

    Update ft_rev_int_tab.c

--- ex07/ft_rev_int_tab.c
+++ ex07/ft_rev_int_tab.c
@@ -28,7 +28,6 @@ void	ft_rev_int_tab(int *tab, int size)
 	}
 }
 
-
 // int	main(void)
 // {
 // 	int		tab[4] = {50, 10, 20, 30};
@@ -44,31 +43,3 @@ void	ft_rev_int_tab(int *tab, int size)
 // 		i++;
 // 	}
 // }
-
-#include <stdio.h>
-
-void	ft_rev_int_tab(int *tab, int size);
-
-void	print_array(int *arr, int size)
-{
-	int	i;
-
-	i = 0;
-	while (i < size)
-		printf("%d ", arr[i++]);
-	printf("\n");
-}
-
-int	main(void)
-{
-	int	empty[0];
-	int	test_even[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-	int	test_odd[11] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
-
-	ft_rev_int_tab(empty, 0);
-	ft_rev_int_tab(test_even, 10);
-	ft_rev_int_tab(test_odd, 11);
-	print_array(empty, 0);
-	print_array(test_even, 10);
-	print_array(test_odd, 11);
-}
