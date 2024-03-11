fc1fb415ccd463e6a324b89a76aeabfca5d1bfdc
marciochamma <82783953+marciochamma@users.noreply.github.com>
2024-03-10 23:44:38 -0300

    Update ft_ultimate_ft.c

--- ex01/ft_ultimate_ft.c
+++ ex01/ft_ultimate_ft.c
@@ -10,13 +10,13 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include<unistd.h>
+#include <stdio.h>
+
 void	ft_ultimate_ft(int *********nbr)
 {
 	*********nbr = 42;
 }
-
-// #include<unistd.h>
-// #include <stdio.h>
 
 // int	main(void)
 // {
