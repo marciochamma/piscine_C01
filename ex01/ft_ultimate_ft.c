1ff2522397b5e59016cfc0bfd783257ea5c49a23
marciochamma <82783953+marciochamma@users.noreply.github.com>
2024-03-10 23:36:32 -0300

    Update ft_ultimate_ft.c

--- ex01/ft_ultimate_ft.c
+++ ex01/ft_ultimate_ft.c
@@ -10,39 +10,39 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include<unistd.h>
-#include <stdio.h>
-
 void	ft_ultimate_ft(int *********nbr)
 {
 	*********nbr = 42;
 }
 
-int	main(void)
-{
-	int	aux;
-	int	*nbr1;
-	int	**nbr2;
-	int	***nbr3;
-	int	****nbr4;
-	int	*****nbr5;
-	int	******nbr6;
-	int	*******nbr7;
-	int	********nbr8;
-	int	*********nbr;
+// #include<unistd.h>
+// #include <stdio.h>
 
-	aux = 0;
-	nbr1 = &aux;
-	nbr2 = &nbr1;
-	nbr3 = &nbr2;
-	nbr4 = &nbr3;
-	nbr5 = &nbr4;
-	nbr6 = &nbr5;
-	nbr7 = &nbr6;
-	nbr8 = &nbr7;
-	nbr = &nbr8;
+// int	main(void)
+// {
+// 	int	aux;
+// 	int	*nbr1;
+// 	int	**nbr2;
+// 	int	***nbr3;
+// 	int	****nbr4;
+// 	int	*****nbr5;
+// 	int	******nbr6;
+// 	int	*******nbr7;
+// 	int	********nbr8;
+// 	int	*********nbr;
 
-	printf("valor anterior: %d\n", aux);
-	ft_ultimate_ft(nbr);
-	printf("valor modificado: %d", aux);
-}
+// 	aux = 0;
+// 	nbr1 = &aux;
+// 	nbr2 = &nbr1;
+// 	nbr3 = &nbr2;
+// 	nbr4 = &nbr3;
+// 	nbr5 = &nbr4;
+// 	nbr6 = &nbr5;
+// 	nbr7 = &nbr6;
+// 	nbr8 = &nbr7;
+// 	nbr = &nbr8;
+
+// 	printf("valor anterior: %d\n", aux);
+// 	ft_ultimate_ft(nbr);
+// 	printf("valor modificado: %d", aux);
+// }
